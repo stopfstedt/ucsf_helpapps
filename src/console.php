0a6cc8ba1c2d9d84d8aeb3012c448c0a5dfbd6c6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-04-13 23:16:29 -0700

    removed dummy command from console.

--- src/console.php
+++ src/console.php
@@ -9,17 +9,8 @@ use Symfony\Component\Console\Input\InputOption;
 $console = new Application('My Silex Application', 'n/a');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
-$console
-    ->register('my-command')
-    ->setDefinition(array(
-        // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-    ))
-    ->setDescription('My command description')
-    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-        // do something
-    })
-;
 
+// ip verify
 $console
     ->register('ip-verify')
     ->setDefinition(array(
